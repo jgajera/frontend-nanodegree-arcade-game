a1facb3ccd490b702a584c254940d3eb50420f84
Janki <jnkdesignhouse@gmail.com>
2018-07-19 11:23:55 -0600

    cleaned up comments, added pseudocode to help plan code

--- js/app.js
+++ js/app.js
@@ -35,8 +35,8 @@ class Enemy {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-
     this.sprite = 'images/enemy-bug.png';
+
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     this.update = function(dt) {
@@ -44,6 +44,7 @@ class Enemy {
       // which will ensure the game runs at the same speed for
       // all computers.
     };
+
     // Draw the enemy on the screen, required method for game
     this.render = function() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -52,8 +53,8 @@ class Enemy {
 }
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// This class requires an update(), render() and a handleInput() method
+// since we are letting the user control the player only.
 
 // // player class function
 class Player {
@@ -64,28 +65,31 @@ class Player {
     this.y;
     this.sprite = 'images/char-pink-girl.png';
     // ....... and methods
-    this.update = function(dt) {}
+    this.update = function(dt) {};
     this.render = function() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+    };
     this.handleInput = function() {
-
-    }
+      // player must move depending on what key the user hit
+    };
   }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+// Instantiate objects.
+// ... Place the player object in a variable called player
 var player = new Player;
+// should enemies have different dt multipliers so they move at various speeds?
 var enemy1 = new Enemy;
 var enemy2 = new Enemy;
 var enemy3 = new Enemy;
 var enemy4 = new Enemy;
+// ... Place all enemy objects in an array called allEnemies
 var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+// Listener function for key presses. Sends the keys to your
+// Player.handleInput() method. Don't modify this!!!
 document.addEventListener('keyup', function(e) {
   var allowedKeys = {
     37: 'left',
