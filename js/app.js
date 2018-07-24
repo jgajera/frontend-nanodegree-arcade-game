9355f8e10118bbc40e3e2474ee98344495439630
Janki <jnkdesignhouse@gmail.com>
2018-07-24 10:06:44 -0600

    fixed JS error from end of game

--- js/app.js
+++ js/app.js
@@ -2,48 +2,54 @@
 
 // when enemy hits edge of the playing field, it should disappear
 class Enemy {
- constructor(x, y, dt) {
-  // function to generate a random whole number for randomizing enemy speeds
-  function getRandomInt(min, max) {
-   //The maximum is exclusive and the minimum is inclusive
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   return Math.floor(Math.random() * (max - min)) + min;
+  constructor(x, y, dt) {
+    // function to generate a random whole number for randomizing enemy speeds
+    function getRandomInt(min, max) {
+      //The maximum is exclusive and the minimum is inclusive
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    // Variables applied to each of our instances go here,
+    this.x = x;
+    this.y = y;
+    // init speeds so all enemies are instantiated with random speeds
+    const speed = getRandomInt(75, 205);
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    this.update = function(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+
+      // if enemy is within the right boundary, move em right!
+      if (this.x < 500) {
+        this.x += speed * dt;
+      }
+
+      // if enemy reached the right boundary, reset them to the left side again
+      if (this.x >= 500) {
+        this.x = x - 100;
+      }
+    };
+
+    // Draw the enemy on the screen, required method for game
+    this.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
   }
-  // Variables applied to each of our instances go here,
-  this.x = x;
-  this.y = y;
-  // init speeds so all enemies are instantiated with random speeds
-  const speed = getRandomInt(75, 205);
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
-  this.update = function(dt) {
-   // You should multiply any movement by the dt parameter
-   // which will ensure the game runs at the same speed for
-   // all computers.
-
-   // if enemy is within the right boundary, move em right!
-   if (this.x < 500) {
-    this.x += speed * dt;
-   }
-
-   // if enemy reached the right boundary, reset them to the left side again
-   if (this.x >= 500) {
-    this.x = x - 100;
-   }
-  };
-
-  // Draw the enemy on the screen, required method for game
-  this.render = function() {
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
- }
 }
+
+document.getElementById('modal-close').click();
+document.getElementById('play-again').addEventListener('click', function() {
+  window.location.reload();
+});
+
 
 // player class - This class requires an update(), render() and
 // a handleInput() method since we are letting the user
@@ -51,81 +57,78 @@ class Enemy {
 
 // // player class function
 class Player {
- // .... add constructor function
- constructor(x, y) {
-  // ....... with properties
-  this.x = x;
-  this.y = y;
-  this.sprite = 'images/char-pink-girl.png';
-  let startX = x;
-  let startY = y;
+  // .... add constructor function
+  constructor(x, y) {
+    // ....... with properties
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-pink-girl.png';
+    let startX = x;
+    let startY = y;
+    this.wonGame = false;
 
-  // ....... and methods
-  this.update = function(dt) {
-   // player should move at a constant speed, block-to-block
-   // check for collision... collision function - when player hits // the enemy, the player restarts at the bottom of the grid
+    // ....... and methods
+    this.update = function(dt) {
+      // player should move at a constant speed, block-to-block
+      // check for collision... collision function - when player hits // the enemy, the player restarts at the bottom of the grid
 
-   // ... track the positions of each enemy and player
-   // ... update variables for each entity on position
-   // ... when variables are equal or close, call collision function
-   for (let enemy of allEnemies) {
-    let enemyX = enemy.x;
-    let enemyY = enemy.y;
-    if (
-     this.y === enemyY &&
-     (this.x < Math.ceil(enemyX) + 10 && this.x > Math.floor(enemyX) - 10)
-    ) {
-     this.reset();
-    }
-   }
+      // ... track the positions of each enemy and player
+      // ... update variables for each entity on position
+      // ... when variables are equal or close, call collision function
+      for (let enemy of allEnemies) {
+        let enemyX = enemy.x;
+        let enemyY = enemy.y;
+        if (
+          this.y === enemyY &&
+          (this.x < Math.ceil(enemyX) + 10 && this.x > Math.floor(enemyX) - 10)
+        ) {
+          this.reset();
+        }
+      }
 
-   // check for win by checking position, if reached river, then
-   // toggle congrats modal and pause animation
-   if (this.y === -15) {
-    document.getElementById('modal-link').click();
-    window.cancelAnimationFrame(main);
-   }
-  };
+      // check for win by checking position, if reached river, then
+      // toggle congrats modal and pause animation
+      if (this.y === -15) {
+        this.wonGame = true;
+        document.getElementById('modal-link').click();
+      }
 
-  // renders our player image
-  this.render = function() {
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
+    };
 
-  this.handleInput = function(input) {
-   // player must move a block depending on what key the user
-   // pressed. && conditions handle canvas boundaries for the player
+    // renders our player image
+    this.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 
-   // ... key up && conditional for top of grid
-   if (input === 'up' && this.y > 0) {
-    this.y -= 80;
-    return this.y;
-   }
-   // ... key down && conditional for bottom of grid
-   else if (input === 'down' && this.y < 375) {
-    this.y += 80;
-    return this.y;
-   }
-   // ... key left && conditional for left side of grid
-   else if (input === 'left' && this.x > 0) {
-    this.x -= 100;
-    return this.x;
-   }
-   // ... key right && conditional for right side of grid
-   else if (input === 'right' && this.x < 400) {
-    this.x += 100;
-    return this.x;
-   }
-  };
+    this.handleInput = function(input) {
+      // player must move a block depending on what key the user
+      // pressed. && conditions handle canvas boundaries for the player
 
-  // ... when enemy and player start touching, reset player
-  // back to original position
-  this.reset = function() {
-   this.y = startY;
-   this.x = startX;
-   console.log(this.x, this.y);
-  };
- }
+      // ... key up && conditional for top of grid
+      if (input === 'up' && this.y > 0) {
+        this.y -= 80;
+      }
+      // ... key down && conditional for bottom of grid
+      else if (input === 'down' && this.y < 375) {
+        this.y += 80;
+      }
+      // ... key left && conditional for left side of grid
+      else if (input === 'left' && this.x > 0) {
+        this.x -= 100;
+      }
+      // ... key right && conditional for right side of grid
+      else if (input === 'right' && this.x < 400) {
+        this.x += 100;
+      }
+    };
+
+    // ... when enemy and player start touching, reset player
+    // back to original position
+    this.reset = function() {
+      this.y = startY;
+      this.x = startX;
+    };
+  }
 }
 
 // Instantiate objects.
@@ -137,17 +140,16 @@ const enemy2 = new Enemy(0, 145);
 const enemy3 = new Enemy(0, 225);
 // ... Place all enemy objects in an array called allEnemies
 const allEnemies = [enemy1, enemy2, enemy3];
-console.log(allEnemies);
 
 // Listener function for key presses. Sends the keys to your
 // Player.handleInput() method. Don't modify this!!!
 document.addEventListener('keyup', function(e) {
- var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down'
- };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
- player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -50,7 +50,11 @@ var Engine = (function(global) {
     /* Use the browser's requestAnimationFrame function to call this
      * function again as soon as the browser is able to draw another frame.
      */
-    win.requestAnimationFrame(main);
+    if (player.wonGame === true) {
+      win.cancelAnimationFrame(main);
+    } else {
+      win.requestAnimationFrame(main);
+    }
   }
 
   /* This function does some initial setup that should only occur once,
